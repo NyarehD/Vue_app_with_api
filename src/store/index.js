597eaf2e33b7adc202f9b050b156ead73a963b8c
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-03-30 19:42:05 +0630

    trying to get data with async

--- src/store/index.js
+++ src/store/index.js
@@ -4,34 +4,35 @@ import axios from "axios";
 export default createStore({
     state: {
         articles: [],
-        articlesWithOtherData: [],
+        articlesIncludingMeta: [],
+        articlePage: 1,
         fetchStatus: {
-            type: Boolean,
-            required: true
+            type: Boolean, required: true
         }
-    },
-    getters: {},
-    mutations: {
+    }, getters: {}, mutations: {
         SET_ARTICLES(state, articles) {
-            state.articles = articles.articles;
-            state.articlesWithOtherData = articles;
+            state.articles.push(...articles.articles)
+            state.articlesIncludingMeta = articles;
+        },
+        SET_ARTICLE_PAGE(state) {
+            state.articlePage += 1;
         },
         SET_STATUS(state, status) {
             state.fetchStatus = status
         }
-    },
-    actions: {
-        getPost({commit}) {
-            axios.get("https://newsapi.org/v2/top-headlines?sortBy=popularity&pageSize=30&language=en&apiKey=b04ad0967ac040f6af1753f437d4c3c8")
+    }, actions: {
+        getPost({commit, state}) {
+            axios.get(`https://newsapi.org/v2/top-headlines?sortBy=popularity&pageSize=5&page=${state.articlePage}&language=en&apiKey=b04ad0967ac040f6af1753f437d4c3c8`)
                 .then(response => {
                     if (response.data.status === "ok") {
                         commit("SET_ARTICLES", response.data)
+                        commit("SET_STATUS", response.data.status)
+                        commit("SET_ARTICLE_PAGE")
                     } else {
                         commit("SET_STATUS", response.data.status)
                     }
                 })
         }
-    },
-    modules: {},
+    }, modules: {},
 })
 
