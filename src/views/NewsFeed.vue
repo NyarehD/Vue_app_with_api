e6900588ef9c77370a12c461f37f96c693be8b1f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-03-31 16:45:44 +0630

    loading animation

--- src/views/NewsFeed.vue
+++ src/views/NewsFeed.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="container-md">
-    <div v-if="storedArticles.length>=0" class="row justify-content-center">
+    <div v-if="storedArticles.length >= 0" class="row justify-content-center">
       <news-card v-for="article in storedArticles" :key="article.url" :article="article"></news-card>
     </div>
-    <div class="row justify-content-center loading" @change="getNews">
-      LOL LOADING
-    </div>
     <Waypoint class="row justify-content-center loading" @change="getNews">
-      LOL Loading
+      <div class="flipping-3">
+
+      </div>
     </Waypoint>
   </div>
 </template>
@@ -59,4 +58,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.flipping-3 {
+  width: 40px;
+  height: 40px;
+  transform: perspective(150px) rotateX(0) rotateY(0);
+  animation: f3-1 2s infinite linear,
+  f3-2 3s infinite linear -.5s;
+}
+
+@keyframes f3-1 {
+  50% {
+    transform: perspective(150px) rotateX(180deg) rotateY(0)
+  }
+  100% {
+    transform: perspective(150px) rotateX(180deg) rotateY(180deg)
+  }
+}
+
+@keyframes f3-2 {
+  0%, 33% {
+    background: #337ab7
+  }
+  33.1%, 66% {
+    background: #41B883
+  }
+  66.1%, 100% {
+    background: #34495B
+  }
+}
 </style>
