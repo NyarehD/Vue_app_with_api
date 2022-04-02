ed17dedf0f3606230462ac5c1d87e30e2a7f7a65
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-04-02 20:33:36 +0630

    Changed to fetching news in DetailedNews.vue

--- src/views/DetailedNews.vue
+++ src/views/DetailedNews.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container-md">
+  <div class="container-lg">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <h2 class="text-start fw-bold">{{ currentArticle.title }}</h2>
-        <p class="mb-0">Source : {{ currentArticle.source.name }}</p>
-        <p>By {{ currentArticle.author }} at {{ currentArticle.publishedAt }}</p>
+        <h2 class="text-start fw-bold">{{ article.title }}</h2>
+        <p class="mb-0">Source : {{ article.source.name }}</p>
+        <p>By {{ article.author }} at {{ article.publishedAt }}</p>
       </div>
       <div class="col-md-9">
-        <img :src="currentArticle.urlToImage" alt class="w-100"/>
+        <img :src="article.urlToImage" alt class="w-100"/>
       </div>
       <div class="col-md-8">
         <p class="text-start">
-          {{ currentArticle.content }}
-          <a :href="currentArticle.url" target="_blank">Read more</a>
+          {{ article.content }}
+          <a :href="article.url" target="_blank">Read more</a>
         </p>
       </div>
     </div>
@@ -20,22 +20,24 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "DetailedNews",
   data() {
-    return {}
+    return {
+      articles: Object,
+      article: Object
+    }
   },
   beforeMount() {
-    if (this.$store.state.articles.length === 0) {
-      this.$store.dispatch("getPost");
-    }
+    axios.get(`https://newsapi.org/v2/everything?q=${this.$route.params.title}&searchIn=title&apiKey=b04ad0967ac040f6af1753f437d4c3c8`)
+        .then(response => {
+          this.articles = response.data;
+          this.article = response.data.articles[0]
+        })
     window.document.title = this.$route.params.title;
   },
-  computed: {
-    currentArticle() {
-      return this.$store.state.articles.filter(article => article.title === this.$route.params.title)[0];
-    },
-  }
 }
 </script>
 
